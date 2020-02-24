c19d0343c3174d9db1a6aedf83f3687a19b1a77c
MohamedElmdary <engm5081@gmail.com>
2020-02-24 19:04:36 +0200

    Worms Evolution

--- Worms Evolution.cpp
+++ Worms Evolution.cpp
@@ -1,8 +1,40 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
 
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            int sum = a[i] + a[j];
+            for (int k = 0; k < n; k++)
+            {
+                if (k == i || k == j)
+                {
+                    continue;
+                }
+                if (sum == a[k])
+                {
+                    printf("%d %d %d\n", k + 1, j + 1, i + 1);
+                    return 0;
+                }
+            }
+        }
+    }
+
+    printf("-1\n");
     return 0;
 }
